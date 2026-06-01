dae55d5ba037c845eb7059e89c11d732edc8b4b8
ancient03 <rizqieadriananto@gmail.com>
2026-06-01 21:43:14 +0700

    State in Component with Hooks

--- src/components/Elements/Card.jsx
+++ src/components/Elements/Card.jsx
@@ -4,12 +4,12 @@ function Card(props) {
   const { title, link = false, desc } = props;
 
   return (
-    <div className="flex flex-col">
+    <div className="h-full flex flex-col">
       <div className="flex justify-between items-center text-gray-02 mb-2">
         <div className="text-2xl">{title}</div> 
         {link && <div className="text-xs">View All</div>}
       </div>
-      <div className="bg-white rounded-lg px-6 py-5 shadow-xl">
+      <div className="flex-1 bg-white rounded-lg px-6 py-5 shadow-xl">
         {desc}
       </div>
     </div>
